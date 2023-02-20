0b1d1b9541a3bdb8958d6b4920cfc6eaeb7958c2
Suraj Barole <103420352+Surajbarole@users.noreply.github.com>
2023-02-20 06:09:00 +0000

    Initial commit

---
+++ sol4.js
@@ -0,0 +1,10 @@
+// Problem 4: Print the average of even numbers from 1 to 100 (both included)
+let sum=0;
+let count=0;
+for(i=1;i<=5;i++){
+  if(i%2==0){
+    sum+=i
+    count++
+  }
+}
+console.log(sum/count)
